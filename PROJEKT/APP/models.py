fa8b788d3a8b1d6312a4d496851c33ab154040e0
GergoFaludy <faludygergo@gmail.com>
2022-04-08 13:00:55 +0200

    hellodjango kesz

--- PROJEKT/APP/models.py
+++ PROJEKT/APP/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class MODELNAME(models.Model):
+    """Model definition for MODELNAME."""
+
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for MODELNAME."""
+
+        verbose_name = 'MODELNAME'
+        verbose_name_plural = 'MODELNAMEs'
+
+    def __str__(self):
+        """Unicode representation of MODELNAME."""
+        pass
